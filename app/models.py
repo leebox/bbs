ebc410add303fb813875172bfb8c717c4a24c5e5
Ryan Lee <lihygaasd@gmail.com>
2014-05-17 00:55:46 +0200

    created topics database

--- app/models.py
+++ app/models.py
@@ -2,7 +2,8 @@
 from flask.ext.sqlalchemy import SQLAlchemy
 #加密保存密码
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from datetime import datetime
+from webhelpers.date import time_ago_in_words
 db = SQLAlchemy()
 
 
@@ -12,6 +13,7 @@ class User(db.Model):
     username = db.Column(db.String(100))
     email = db.Column(db.String(120), unique=True)
     pwdhash = db.Column(db.String(54))
+    created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __init__(self, username, email, password):
         self.username = username.title()
@@ -23,5 +25,34 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.pwdhash, password)
+
+
+class Topics():
+    __tablename__ = 'topics'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+    updated = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        index=True,
+    )
+
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return '<Topic: %s>' % self.id
+
+    @property
+    def created_in_words(self):
+        return time_ago_in_words(self.created)
+
+
 
 
